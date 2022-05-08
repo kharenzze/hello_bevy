0a291d794df8cb9f806176f70b26c28e75af9d3f
Paolo Raez <str237@gmail.com>
2022-05-08 15:08:44 +0200

    Remove hello_world

--- src/main.rs
+++ src/main.rs
@@ -33,17 +33,10 @@ fn main() {
     .add_plugin(HelloPlugin);
 }
 
-fn hello_world() {
-  println!("Hello, world!");
-}
-
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
   fn build(&self, app: &mut App) {
-    app
-      .add_startup_system(add_people)
-      .add_system(hello_world)
-      .add_system(greet_people);
+    app.add_startup_system(add_people).add_system(greet_people);
   }
 }
