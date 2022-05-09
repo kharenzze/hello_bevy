61a809cf67aefbf6a40ae4eb487d353500f3cdd0
Paolo Raez <str237@gmail.com>
2022-05-09 11:01:44 +0200

    Plugins

--- src/main.rs
+++ src/main.rs
@@ -21,22 +21,30 @@ fn add_people(mut commands: Commands) {
     .insert(Name("Zayna Nieves".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-  for name in query.iter() {
-    println!("hello {}!", name.0);
-  }
-}
-
 fn main() {
   App::new()
     .add_plugins(DefaultPlugins)
-    .add_plugin(HelloPlugin);
+    .add_plugin(HelloPlugin)
+    .run();
 }
 
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
   fn build(&self, app: &mut App) {
-    app.add_startup_system(add_people).add_system(greet_people);
+    app
+      .insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+      .add_startup_system(add_people)
+      .add_system(greet_people);
+  }
+}
+
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+  if timer.0.tick(time.delta()).just_finished() {
+    for name in query.iter() {
+      println!("hello {}!", name.0);
+    }
   }
 }
