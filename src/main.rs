9c2babfcffa9c38208276ab43ade5fac6d4a3c3b
Paolo Raez <str237@gmail.com>
2022-05-10 23:00:52 +0200

    Moving player

--- src/main.rs
+++ src/main.rs
@@ -23,19 +23,21 @@ impl Plugin for PlayerPlugin {
 struct Player;
 
 impl PlayerPlugin {
-  fn player_system(query: Query<(&Name, &Transform), With<Player>>) {
-    for p in query.iter() {
-      println!("{}", &p.0 .0);
+  fn player_system(mut query: Query<(&Name, &mut Transform), With<Player>>) {
+    for (name, mut transform) in query.iter_mut() {
+      transform.translation.x += 2.;
     }
   }
 }
+
+const PLAYER_COLOR: Color = Color::rgb(0.25, 0.25, 0.75);
 
 fn setup(mut commands: Commands) {
   commands.spawn_bundle(OrthographicCameraBundle::new_2d());
   commands
     .spawn_bundle(SpriteBundle {
       sprite: Sprite {
-        color: Color::rgb(0.25, 0.25, 0.75),
+        color: PLAYER_COLOR,
         custom_size: Some(Vec2::new(50.0, 50.0)),
         ..default()
       },
