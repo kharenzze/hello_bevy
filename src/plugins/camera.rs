9867940ffebcff8714069d6bd925099b35b7fca6
Paolo Raez <str237@gmail.com>
2022-05-14 12:57:49 +0200

    Store mouse world position

--- src/plugins/camera.rs
+++ src/plugins/camera.rs
@@ -1,5 +1,6 @@
 use crate::resources::MousePos;
 use bevy::prelude::*;
+use bevy::render::camera::RenderTarget;
 
 #[derive(Component)]
 struct MainCamera;
@@ -10,12 +11,34 @@ impl Plugin for CameraPlugin {
   fn build(&self, app: &mut App) {
     app
       .insert_resource(MousePos::default())
+      .add_startup_system(camera_startup_system)
       .add_system(camera_system);
   }
 }
 
+fn camera_startup_system(mut cmd: Commands) {
+  cmd.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+type CameraQuery<'a> = (&'a Camera, &'a GlobalTransform);
 fn camera_system(
   wnds: Res<Windows>,
-  q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+  mut mouse_pos: ResMut<MousePos>,
+  q_camera: Query<CameraQuery, With<MainCamera>>,
 ) {
+  let (camera, camera_transform): CameraQuery = q_camera.single();
+  let wnd = if let RenderTarget::Window(id) = camera.target {
+    wnds.get(id).unwrap()
+  } else {
+    wnds.get_primary().unwrap()
+  };
+
+  if let Some(screen_pos) = wnd.cursor_position() {
+    let window_size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
+    let ndc = (screen_pos / window_size) * 2.0 - Vec2::ONE;
+    let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix.inverse();
+    let world_pos = ndc_to_world.project_point3(ndc.extend(-1.0));
+    let world_pos: Vec2 = world_pos.truncate();
+    mouse_pos.set(world_pos);
+  }
 }
