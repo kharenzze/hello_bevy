f201094709daa12ce360f494a481254f154b6311
Paolo Raez <str237@gmail.com>
2022-05-14 12:08:27 +0200

    MousePos

--- src/resources.rs
+++ src/resources.rs
@@ -1,1 +1,18 @@
 use bevy::prelude::*;
+use std::ops::{Deref, DerefMut};
+
+struct MousePos(Vec2);
+
+impl Deref for MousePos {
+  type Target = Vec2;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+
+impl DerefMut for MousePos {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    &mut self.0
+  }
+}
